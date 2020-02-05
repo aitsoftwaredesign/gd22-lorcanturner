193884ed9700067b5f9047a1f35f2a3bb03c98af
lorcanturner <59914732+lorcanturner@users.noreply.github.com>
2020-02-05 10:55:25 +0000

    challange 1

--- TopDown/Assets/Scenes/code/moving.cs
+++ TopDown/Assets/Scenes/code/moving.cs
@@ -6,8 +6,9 @@ using UnityEngine;
 
 public class moving : MonoBehaviour
 {
+    float distance;
     public Transform target;
-    float speed;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,24 +18,43 @@ public class moving : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
+        Debug.Log(distance + "you are units away from enemy");
+ 
         float speedDelta = speed * Time.deltaTime;
+
+        
         Vector3 newPosition = myMoveTowards(speedDelta);
+        transform.position = newPosition;
+       
+        
+       
+       
     }
 
     Vector3 myMoveTowards(float spd )
     {
-
-
+        
         Vector3 currentPosition = transform.position;
         Vector3 destinationPosition = target.transform.position;
         Vector3 range = destinationPosition - currentPosition;
         
 
         Debug.DrawRay(currentPosition, range, Color.red);
+       
 
+       // length of the normilised line
         Debug.DrawRay(currentPosition, range.normalized, Color.blue);
 
-        Vector3 newPosition = currentPosition;
+
+        //move depeed delta along the normalised range
+        
+        Vector3 velocity = range.normalized * spd;
+   
+
+
+        Vector3 newPosition = currentPosition + velocity;
         return newPosition;
     }
 }
---
+++ TopDown/Assets/Scenes/code/topdownCode/BasicChase.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicChase : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        follow();
+        
+    }
+
+    public void follow()
+    {
+        float speedDelta = speed * Time.deltaTime;
+
+        if(transform.position.x < target.transform.position.x)
+        {
+            transform.Translate(speedDelta , 0, 0);
+        }
+        else
+        {
+            transform.Translate(-speedDelta, 0, 0);
+        }
+
+        if (transform.position.y < target.transform.position.y)
+        {
+            transform.Translate(0,speedDelta , 0);
+        }
+        else
+        {
+            transform.Translate(0, -speedDelta, 0);
+        }
+    }
+}
