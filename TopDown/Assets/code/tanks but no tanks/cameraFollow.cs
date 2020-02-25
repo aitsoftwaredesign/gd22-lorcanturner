43a77a2df41ba753928b0b729a040d1ea659ba8d
lorcanturner <59914732+lorcanturner@users.noreply.github.com>
2020-02-25 15:38:58 +0000

    tank!!!!!!!!!!!!!!

---
+++ TopDown/Assets/code/tanks but no tanks/cameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    public Transform tank;
+    public Vector3 Offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(tank != null)
+        {
+            transform.position = tank.position + Offset;
+        }
+    }
+}
---
+++ TopDown/Assets/code/tanks but no tanks/tankwheelmotion.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tankwheelmotion : MonoBehaviour
+{
+    public WheelJoint2D wheel;
+    public float speed = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wheel = GetComponent<WheelJoint2D>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        JointMotor2D motor;
+        motor = wheel.motor;
+
+        if(Input.GetKeyDown("d"))
+        {
+            motor.motorSpeed = speed;
+            wheel.motor = motor;
+        }
+        if (Input.GetKeyDown("a"))
+        {
+           motor.motorSpeed = -speed;
+            wheel.motor = motor;
+        }
+        else if(Input.GetKeyUp("d")||Input.GetKeyUp("a"))
+        {
+            motor.motorSpeed = 0;
+
+            wheel.motor = motor;
+        }
+        else if(Input.GetKeyDown("s"))
+        {
+            motor.motorSpeed = 0;
+
+            wheel.motor = motor;
+        }
+        Debug.Log(speed);
+    }
+}
