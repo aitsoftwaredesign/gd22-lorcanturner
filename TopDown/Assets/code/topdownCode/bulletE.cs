5f7b185ba47ac137b8eb8d90474f9e1c9f00007a
Lorcan Turner <A00264376@studnet.ait.ie>
2020-02-27 20:47:46 +0000

    update to shooting enemy
    enemy bullets nolonger kill other enemies

---
+++ TopDown/Assets/code/topdownCode/bulletE.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletE : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("preditor"))
+        {
+
+            Destroy(gameObject);
+
+
+        }
+
+        if (collision.gameObject.CompareTag("obticle"))
+        {
+            Destroy(gameObject);
+
+        }
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            health.playerLives -= 1;
+            Destroy(gameObject);
+            //animation for bullet will be added
+            if (health.playerLives < 1)
+            {
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+}
